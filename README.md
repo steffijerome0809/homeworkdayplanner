4c66022d0c158d7e16ea0ecfbc205c6414d16e66
Steffi Joel <steffijerome0809>
2020-05-03 23:40:51 -0400

    updated readme file and project

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # homeworkdayplanner
+
+## Instructions
+A workday event planner is created which displays the time from morning 8 am to evening 5pm. 
+
+Event are entered for every hour and the save button is provided to save the events. Additionaly events can be deleted specific.
+
+The event are updated with color coding to indicate if the current event is in the past or in the future. Moment.js is used to parse the event id which is the time and its validated.
+
+URL
+
+https://github.com/steffijerome0809/homeworkdayplanner
+https://steffijerome0809.github.io/homeworkdayplanner/
+
--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,52 +1,46 @@
-$("#showTime").html(moment().format('dddd, MMMM Do'))
+$("#showTime").html(moment().format("dddd, MMMM Do"));
 
 //an event is written foa specific time and is saved in the local storage
 $(".saveIcon").click(function () {
-    var time = $(this).parent('tr').prop('id')
-    var textInput = $(this).parent('tr').children(".writeEvent").html();
+  var time = $(this).parent("tr").prop("id");
+  var textInput = $(this).parent("tr").children(".writeEvent").html();
 
-    //console.log("evnt written" + textInput);
-    localStorage.setItem(time, textInput);
+  //console.log("evnt written" + textInput);
+  localStorage.setItem(time, textInput);
 });
 
 // For each time id the html is retreived and the time is compared with its past or future time
 $("tr").each(function () {
-    var getEvent = localStorage.getItem(this.id)
-    console.log(this.id);
-    $(this).children('.writeEvent').html(getEvent);
-    compareTime(this.id);
+  var getEvent = localStorage.getItem(this.id);
+  console.log(this.id);
+  $(this).children(".writeEvent").html(getEvent);
+  compareTime(this.id);
 });
 
 //deletes the event written for a specific time
 
 $(".deleteEvent").click(function () {
-    var time = $(this).parent('tr').prop('id')
-    var deletetext = $(this).parent('tr').children(".writeEvent").html();
-    localStorage.removeItem(time, deletetext);
-    //deletetext.listview('refresh');
-
+  var time = $(this).parent("tr").prop("id");
+  var deletetext = $(this).parent("tr").children(".writeEvent").html();
+  localStorage.removeItem(time, deletetext);
+  //deletetext.listview('refresh');
 });
-
-
-
 
 // Comapares the event written with the current time and updates to whether its a past or a future event.
 
 function compareTime(timeId) {
-    var time = $("#" + timeId).children('.writeEvent')
+  var time = $("#" + timeId).children(".writeEvent");
 
-    // console.log("loading" + ":" + timeId);
+  // console.log("loading" + ":" + timeId);
 
-    if (((parseInt(moment().format('H'))) > (parseInt(timeId.slice(1))))) {
-        // past #c0b9b9
-        time.attr("style", "background-color: #c0b9b9;")
-    }
-    else if (((parseInt(moment().format('H'))) < (parseInt(timeId.slice(1))))) {
-        // future #9ae591
-        time.attr("style", "background-color: #9ae591;")
-    }
-    else {
-        // current #f36060
-        time.attr("style", "background-color: #f36060;")
-    }
+  if (parseInt(moment().format("H")) > parseInt(timeId.slice(1))) {
+    // past #c0b9b9
+    time.attr("style", "background-color: #c0b9b9;");
+  } else if (parseInt(moment().format("H")) < parseInt(timeId.slice(1))) {
+    // future #9ae591
+    time.attr("style", "background-color: #9ae591;");
+  } else {
+    // current #f36060
+    time.attr("style", "background-color: #f36060;");
+  }
 }
